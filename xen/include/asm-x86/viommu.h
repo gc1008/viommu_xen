2299f92735009f91f14b133fc6b31b054c6755f6
Lan Tianyu <tianyu.lan@intel.com>
2017-10-29 21:42:37 -0400

    VIOMMU: Add irq request callback to deal with irq remapping
    This patch is to add irq request callback for platform implementation
    to deal with irq remapping request.
    
    Signed-off-by: Lan Tianyu <tianyu.lan@intel.com>
    Signed-off-by: Chao Gao <chao.gao@intel.com>

---
+++ xen/include/asm-x86/viommu.h
@@ -0,0 +1,54 @@
+/*
+ * include/asm-x86/viommu.h
+ *
+ * Copyright (c) 2017 Intel Corporation.
+ * Author: Lan Tianyu <tianyu.lan@intel.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef __ARCH_X86_VIOMMU_H__
+#define __ARCH_X86_VIOMMU_H__
+
+/* IRQ request type */
+enum viommu_irq_request_type {
+    VIOMMU_REQUEST_IRQ_MSI = 0,
+    VIOMMU_REQUEST_IRQ_APIC = 1
+};
+
+struct arch_irq_remapping_request
+{
+    union {
+        /* MSI */
+        struct {
+            uint64_t addr;
+            uint32_t data;
+        } msi;
+        /* Redirection Entry in IOAPIC */
+        uint64_t rte;
+    } msg;
+    uint16_t source_id;
+    enum viommu_irq_request_type type;
+};
+
+#endif /* __ARCH_X86_VIOMMU_H__ */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
